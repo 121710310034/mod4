c7c74c5801b67148e7b0a8fe4f5fef6945fe4769
121710310034 <65111121+121710310034@users.noreply.github.com>
2020-06-01 21:15:58 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,9 +1,9 @@
 (function (window){
-	var byeSpeaker = {};
+	var byeSpeaker = new object();
 	var speakWord= "Good Bye";
-	byeSpeaker.speak= function (name){
+	byeSpeaker.speak= function speak(name){
 		console.log(speakWord + " " + name);
-	}
+	};
 
 	window.byeSpeaker=byeSpeaker;
 })(window);
