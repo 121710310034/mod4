da297d49b0e08f59c8e165d814512619db30f87c
121710310034 <65111121+121710310034@users.noreply.github.com>
2020-06-01 21:23:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-(function(window){
+(function(){
 
 	var names=["Yaakov", "John", "Jen", "Jason", "Paul", 
 	"Frank", "Larry", "Paula", "Laura", "Jim" ];
 
-	for(var i in names){
+	for(var i = 0; i < names.length; i++){
 
-		var firstLetter=((names[i]).charAt(0).toLowerCase());
+		var firstLetter= names[i].charAt(0).toLowerCase();
 
-		if(firstLetter == 'j'){
-			window.byeSpeaker.speak(names[i]);
+		if(firstLetter === 'j'){
+			byeSpeaker.speak(names[i]);
 		} else {
-            window.helloSpeaker.speak(names[i]);
+            helloSpeaker.speak(names[i]);
 		}
 
 	}
-})(window);
+})();
